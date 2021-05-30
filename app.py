46561b53b1845cb85c7bc20df3a5fae15a0de702
jasmeet singh <jasmeetsaini801@gmail.com>
2021-05-30 20:43:18 +0530

    first commit

--- app.py
+++ app.py
@@ -1,120 +1,148 @@
-from flask import Flask
-from flask import jsonify, request
+# Name:  Jasmeet Singh
+# Student Id : 
+
+import flask
+from flask import request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String , Float
+import os
+from flask_marshmallow import Marshmallow
 from flask_pymongo import PyMongo
+from flask import Flask, render_template, url_for, request, session, redirect
+import bcrypt
+from pymongo import MongoClient
 
-app= Flask(__name__)
+# flask application object
+app = flask.Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://cst_user:mcit123@cluster0.vo3fn.mongodb.net/jasmeet?retryWrites=true&w=majority"
+app.config["DEBUG"] = True
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'app1.db')
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+@app.route('/mcit/cst-students/all',methods=['GET'])
+def student_all():
+    student_list = Student1.query.all()
+    result =  student_schema.dump(student_list)
+    return jsonify(result)
+
+@app.route('/mcit/cst-students/<int:id>',methods=['GET'])
+def student_id(id):
+
+    results = []
+
+    student_list = Student1.query.all()
+    student_list = student_schema.dump(student_list)
+    for student in student_list:
+        if student['ID'] == id:
+            results.append(student)
+            return jsonify(results)
+
+
+    if not results:
+        return "Error : Id not found"
+    else:       
+        return jsonify(results)
+
+
+
+class Student1(db.Model):
+    __tablename__ = 'students'
+    ID = Column(Integer ,primary_key=True)
+    Name = Column(String)
+    Branch = Column(String)
+    College = Column(String)
+    Batch = Column(String)
+    Program = Column(String)
+    Course =  Column(String)
+    First_Language = Column(String)
+
+
+class StudentSchema1(ma.Schema):
+    class Meta:
+        fields = ('ID','Name','Branch','College','Batch','Program','Course','First_Language')
+
+
+
+student_schema = StudentSchema1()
+student_schema = StudentSchema1(many=True)
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created')
+    
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Databse Dropped')
+
+@app.cli.command('db_seed')
+def db_seed():
+    Jasmeet = Student1(Name = 'Jasmeet Singh',
+                      Branch = 'CS',
+                      College = 'MCIT',
+                      Batch = 'A',
+                      Program = 'CST',
+                      Course = 'Advance Programming',
+                      First_Language = 'Java')
+    Manpreet = Student1(Name = 'Manpreet Singh',
+                      Branch = 'CS',
+                      College = 'MCIT',
+                      Batch = 'A',
+                      Program = 'CST',
+                      Course = 'Advance Programming',
+                      First_Language = 'C')      
+
+
+    db.session.add(Jasmeet)
+    db.session.add(Manpreet)
+      
+    db.session.commit()
+    print('Database seeded')
+
+
+app.config["MONGO_URI"] = "mongodb+srv://cst_user:mcit123@cluster0.vo3fn.mongodb.net/jasmeet?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 mongo_client = PyMongo(app)
 
-db = mongo_client.db
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
 
-@app.route("/book")
-def add_one_book():
-    db.books.insert_one({
-	"id": 1,
-    "author": "Chinua Achebe",
-    "country": "Nigeria",
-    "imageLink": "images/things-fall-apart.jpg",
-    "language": "English",
-    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
-    "pages": 209,
-    "title": "Things Fall Apart",
-    "year": 1958
-    })
-    return jsonify(message="sucess")
+    return render_template('index.html')
 
-@app.route("/books")
-def add_many_books():
-    db.books.insert_many([
-    {
-	"id": 1,
-    "author": "Chinua Achebe",
-    "country": "Nigeria",
-    "imageLink": "images/things-fall-apart.jpg",
-    "language": "English",
-    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
-    "pages": 209,
-    "title": "Things Fall Apart",
-    "year": 1958
-    },
-    {
-	"id": 2,  
-    "author": "Hans Christian Andersen",
-    "country": "Denmark",
-    "imageLink": "images/fairy-tales.jpg",
-    "language": "Danish",
-    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
-    "pages": 784,
-    "title": "Fairy tales",
-    "year": 1836
-    },
-    {
-	"id": 3,  
-    "author": "Dante Alighieri",
-    "country": "Italy",
-    "imageLink": "images/the-divine-comedy.jpg",
-    "language": "Italian",
-    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
-    "pages": 928,
-    "title": "The Divine Comedy",
-    "year": 1315
-    },
-    {
-	"id": 4,  
-    "author": "Unknown",
-    "country": "Sumer and Akkadian Empire",
-    "imageLink": "images/the-epic-of-gilgamesh.jpg",
-    "language": "Akkadian",
-    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
-    "pages": 160,
-    "title": "The Epic Of Gilgamesh",
-    "year": -1700
-    },
-    {
-	"id": 5,  
-    "author": "Unknown",
-    "country": "Achaemenid Empire",
-    "imageLink": "images/the-book-of-job.jpg",
-    "language": "Hebrew",
-    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
-    "pages": 176,
-    "title": "The Book Of Job",
-    "year": -600
-    },
-    {
-	"id": 6,  
-    "author": "Unknown",
-    "country": "India/Iran/Iraq/Egypt/Tajikistan",
-    "imageLink": "images/one-thousand-and-one-nights.jpg",
-    "language": "Arabic",
-    "link": "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
-    "pages": 288,
-    "title": "One Thousand and One Nights",
-    "year": 1200
-    },
-    ])
-    return jsonify(message="Books Added SuccessFully")
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo_client.db.users
+    login_user = users.find_one({'name' : request.form['username']})
 
-@app.route("/books_list")
-def get_books():
-    books = db.books.find()
-    response = []
-    for book in books:
-        book['_id'] = str(book['_id'])
-        response.append(book)
-    return jsonify(response)
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
 
-@app.route("/update_book",methods=['GET'])
-def update_book():
-    id = int(request.args['id'])
-    book_response = db.books.update_one({'id' : id},{"$set" : {'author':'Jasmeet'}})
-    return book_response.raw_result
+    return 'Invalid username/password combination'
 
-@app.route("/delete_book",methods=['DELETE'])
-def delete_book():
-    id = int(request.args['id'])
-    book_response = db.books.delete_one({'id':id})
-    return jsonify(message="Deleted Sccuessfully")
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo_client.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 app.run()
